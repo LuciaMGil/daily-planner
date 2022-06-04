a1839e024293fb4245356afd12a3cd16c6c38005
Lucia Gil <luciagil5952@gmail.com>
2022-06-03 20:28:12 -0400

    Added styling and linked bootstrap, moment.js, and script

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Create variable for the current time using moment.js
+var today = moment();
+
+// Format and append current date to header
+$("#currentDate").text(moment().format("dddd MMMM Do"));
